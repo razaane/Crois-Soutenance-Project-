693e33b4f7be76016792128e60eed8bd782bbcd6
razane <wakhidirazane@gmail.com>
2025-11-17 20:16:00 +0000

    added:la partie JS de photo url

--- script.js
+++ script.js
@@ -5,14 +5,45 @@ document.addEventListener("DOMContentLoaded", () => {
     btnAfficher.addEventListener("click", () => modal.classList.remove("hidden"));
     closeModal.addEventListener("click", () => modal.classList.add("hidden"));
 
+    const container = document.getElementById("experience_container");
+    const photoUrlInput = document.getElementById("photo_url");
+    const photoPreview = document.getElementById("photo_preview");
+    const photoText = document.getElementById("text_container_photo");
 
+    let employees = JSON.parse(localStorage.getItem("employees")) || [];
 
+    photoUrlInput.addEventListener("input", () => {
+        const url = photoUrlInput.value.trim();
+        if (!url) {
+            photoPreview.classList.add("hidden");
+            photoText.classList.remove("hidden");
+            return;
+        }
+        photoPreview.src = url;
+        photoPreview.classList.remove("hidden");
+        photoText.classList.add("hidden");
+    });
 
+    container.addEventListener("click", (e) => {
+        if (!e.target.classList.contains("btn-add")) return;
 
+        const newExp = document.createElement("div");
+        newExp.classList.add("flex", "items-center", "gap-2", "experience-item");
+        newExp.innerHTML = `
+            <textarea class="experience-input border border-2 rounded-lg w-full p-2 text-sm" rows="1"
+                placeholder="Décrire une autre expérience"></textarea>
+            <button type="button"
+                class="btn-add bg-green-500 hover:bg-green-400 text-white font-bold rounded-lg px-3 py-2">+</button>
+        `;
+        container.appendChild(newExp);
+    });
+
+    
     const nameRegex = /^[a-zA-ZÀ-ÿ\s]+$/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const phoneRegex = /^(\+212|0)(6|7)\d{8}$/;
     const urlRegex = /^(https?:\/\/)[\w\-]+(\.[\w\-]+)+/;
+    const form = document.getElementById("formulaire");
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
